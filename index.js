d20d8c15203a7169c4508c4ea7c1b88cb61ee134
Nial Johnson <nialj707@gmail.com>
2023-01-23 11:30:17 -0800

    coomplete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+
+
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
